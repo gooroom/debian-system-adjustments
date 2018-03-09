e245c0990d7bb181e570a1e65478c4850bb6601e
jongkyung.woo <jkwoo@gooroom.kr>
2018-03-09 14:06:54 +0900

    Modified by getting WEBStorage from pam_mount.conf.xml file
    Change-Id: Id8922a171fab5c9400d0926a7e9be2e535c6b944

--- usr/sbin/gooroom-shutdown-trigger.sh
+++ usr/sbin/gooroom-shutdown-trigger.sh
@@ -1,22 +1,37 @@
 #! /bin/bash
+#
+# Check webdav mountpoint and umount it at system shutdown
 
 echo "[Gooroom] Starting gooroom-shutdown-trigger.sh"
 
 sleep 1
 
-## Umount webdav
-mount | grep WEBStorage
+# Check the webdav mountpoint in pam_mount.conf.xml
+# - mountpoint=/home/$USER/WEBStorage
+if [ -e /etc/security/pam_mount.conf.xml ]; then
+    WEBStorage=$(grep path /etc/security/pam_mount.conf.xml \
+                           | awk -F "\"" '{print $6}' \
+                           | awk -F "/" '{print $4}')
+else
+    exit -1;
+fi
 
-if [ $? = 0 ] ; then
-    echo "[Gooroom] umounting WEBStorage"
+if [ -n "$WEBStorage" ]; then
+    mount | grep $WEBStorage
+
+    echo "[Gooroom] umounting $WEBStorage"
     while [ "$(/usr/bin/pgrep xfce)" == "0" ]
     do
         sleep 1
     done
 
-    # TODO.
-    sleep 1
-    for i in $(mount | grep WEBStorage | awk '{print $3}') ; do
+    # TODO. 
+    # Get the actual login id (%USER) with WEBStorage information
+    for i in $(mount | grep $WEBStorage | awk '{print $3}') ; do
         fusermount -u $i
     done
+else
+    exit -1;
 fi
+
+exit 0;
